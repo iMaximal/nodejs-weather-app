a5129918b9f32aa6a47019d09d3a912df42c59b7
Maximal <maxim@infofree.ru>
2018-03-04 19:05:35 +0500

    callbacks basic

---
+++ playground/callbacks.js
@@ -0,0 +1,14 @@
+const getUser = (id, callback) => {
+  const user = {
+    id,
+    name: 'Vikram',
+  }
+
+  setTimeout(() => {
+    callback(user)
+  }, 3000)
+}
+
+getUser(31, (userObject) => {
+  console.log(userObject)
+})
