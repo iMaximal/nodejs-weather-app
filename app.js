00e86197431811d7576289af1ac53936ac1f35b9
Maximal <maxim@infofree.ru>
2018-03-04 19:20:03 +0500

    pretty printing

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyD54W7M8hb_hKeVYG9yEh0z1SJJvbRv59A',
   json: true,
 }, (error, response, body) => {
-  console.log(body)
+  console.log(JSON.stringify(body, undefined, 2))
 })
